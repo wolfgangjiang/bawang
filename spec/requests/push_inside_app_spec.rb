1fd40b414f23cdb1f14b374bed0b0db3dafc2be8
Wolfgang Jiang <wolfgang.jiang@edoctor.cn>
2014-04-09 10:41:53 +0800

    added negative specs for refactored code

--- spec/requests/push_inside_app_spec.rb
+++ spec/requests/push_inside_app_spec.rb
@@ -12,6 +12,8 @@ describe "push_inside_app_spec" do
     page.should have_content "推送管理"
   end
 
+  let(:event_id) { current_path.match(/[0-9a-f]{24}/).to_s }
+
   it "pushes message from admin" do
     fill_in :message, :with => "大家好"
     click_on "群发"
@@ -36,11 +38,29 @@ describe "push_inside_app_spec" do
       click_on "切换状态"
     end
 
-    event_id = current_path.match(/[0-9a-f]{24}/).to_s
     get "/plugins/api/#{event_id}/push_inside_app/poll"
     data = JSON.parse(response.body)
     data.size.should == 1
     data[0]["text"].should == "大家好"
     (Time.parse(data[0]["create_at"]) - Time.now).abs.should < 1
   end
+
+  describe "route security" do
+    it "prevents get request on post route" do
+      path = "/plugins/#{event_id}/push_inside_app/post/toggle"
+
+      expect do
+        get path
+      end.to raise_error ActionController::RoutingError
+    end
+
+    it "prevents api request on page route" do
+      path = "/plugins/api/#{event_id}/push_inside_app/admin"
+
+      expect do
+        get path
+      end.to raise_error ActionController::RoutingError
+    end
+  end
 end
+
